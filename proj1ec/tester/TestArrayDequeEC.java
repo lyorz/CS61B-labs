8a5adce213abb410dc588aab2fa230b21d90a307
lyorz <lyorzorz@163.com>
2024-03-19 20:15:56 +0800

    proj1EC: 修正错误消息

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
+    private String getOperationsList(ArrayDequeSolution<String> a){
+        String s= "";
+        while(!a.isEmpty()){
+            s=s+a.removeFirst();
+        }
+        return s;
+    }
     @Test
     public void RandomTest(){
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        ArrayDequeSolution<String> operations = new ArrayDequeSolution<>();
 
         Assert.assertTrue( sad.isEmpty() );
 
@@ -21,48 +29,46 @@ public class TestArrayDequeEC {
             if( option == 0 ) {
                 sad.addFirst(i);
                 ads.addFirst(i);
+                operations.addLast("addFirst(" + i + ")\n");
             }
             else if( option == 1) {
                 sad.addLast(i);
                 ads.addLast(i);
+                operations.addLast("addLast(" + i + ")\n");
             }
             else if ( option == 2 ) {
                 if ( ads.isEmpty() || sad.isEmpty() ) { continue; }
                 int idx = StdRandom.uniform(0, sad.size() );
                 Integer expected = ads.get( idx );
                 Integer actual = sad.get( idx );
-                if ( actual != null ){
-                    Assert.assertEquals( "Expect that the " + idx + "th " + "value equals "+ expected + ", but got " + actual + ".\n",
-                            expected,
-                            actual);
-                }
-
+                operations.addLast("get(" + idx + ")\n");
+                Assert.assertEquals( getOperationsList(operations),
+                        expected,
+                        actual);
             }
             else if ( option == 3 ) {
                 if ( ads.isEmpty() || sad.isEmpty() ) { continue; }
                 Integer expected = ads.removeFirst();
                 Integer actual = sad.removeFirst();
-                if ( actual != null ) {
-                    Assert.assertEquals("Expect that the first value equals " + expected + ", but got " + actual + ".\n",
-                            expected,
-                            actual);
-                }
+                operations.addLast("removeFirst()\n");
+                Assert.assertEquals(getOperationsList(operations),
+                        expected,
+                        actual);
             }
             else {
                 if ( ads.isEmpty() || sad.isEmpty() ) { continue; }
                 Integer expected = ads.removeLast();
                 Integer actual = sad.removeLast();
-                if ( actual != null ) {
-                    Assert.assertEquals("Expect that the last value equals " + expected + ", but got " + actual + ".\n",
-                            expected,
-                            actual);
-                }
+                operations.addLast("removeLast()\n");
+                Assert.assertEquals(getOperationsList(operations),
+                        expected,
+                        actual);
             }
         }
 
         Integer expected = ads.size();
         Integer actual = sad.size();
-        Assert.assertEquals( "Expect that size equals " + expected + ", but got " + actual + ".\n",
+        Assert.assertEquals( getOperationsList(operations),
                 expected,
                 actual);
     }
